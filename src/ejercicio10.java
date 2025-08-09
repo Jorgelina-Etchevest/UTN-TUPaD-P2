d103f25af4be6cf42581b09c9225b3a0fab593bb
Jorgelina-Etchevest <etchevestjor@gmail.com>
2025-08-09 11:05:11 -0300

    "Resolucion nuevos ejercicios"

---
+++ src/ejercicio10.java
@@ -0,0 +1,12 @@
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int resultado = a / b; 
+        
+        System.out.println("Resultado: " + resultado);
+
+    }
+    
+}
